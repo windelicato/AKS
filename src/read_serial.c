e7d80563cead1c7f834fc74ce7ae109267f5582f
wei001 <wei001@bucknell.edu>
2013-02-12 14:21:49 +0000

    Update

--- src/read_serial.c
+++ src/read_serial.c
@@ -1,22 +1,25 @@
-#include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
+#include<fcntl.h>
 
 int main(int argc, const char *argv[])
 {
-	
+
 	int usbdev; /* handle to FTDI device */
 	char command[5]; /* buffer of data to send */
 	char response[20]; /* receive buffer */
+	//system("stty -F /dev/ttyUSB0 115200 cs8 -cstopb -parity -icanon min 1 time 1");
 
-	//system("stty -F /dev/ttyUSB0 9600 cs8 -cstopb -parity -icanon min 1 time 1");
+	if ( usbdev = open("/dev/ttyUSB0", O_RDWR) < 0) {
+		perror("Unable to open device");
+		exit(-1);
+	}
 
-	usbdev = open("/dev/ttyUSB0", O_RDWR);
-
-	write(usbdev, command, 2);
+	//write(usbdev, command, 2);
 	read(usbdev, response, 1);
-	close(usbdev);
+	close(usbdev);	
+
+	printf("%s\n",response);
 
 	return 0;
 }
-
-
