928cbc2dfb961e0579fd2a501ede993c3bb67102
root <root@localhost.localdomain>
2013-02-13 16:54:27 -0500

    Unstable test

--- src/read_serial.c
+++ src/read_serial.c
@@ -1,6 +1,13 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<fcntl.h>
+#include<math.h>
+
+#define WEIGHTS 5
+#define AVGS 20
+#define DIFF 0.01
+
+float avg = 0;
 
 int main(int argc, const char *argv[])
 {
@@ -13,31 +20,65 @@ int main(int argc, const char *argv[])
 
 	printf("Reading...\n");
 
-	while(1){
-		char buff[100];
-		float weights[10];
-		int i;
-		for(i=0; i<10; fgets(buff, 100, file)) {
-			float weight;
-			fgets(buff, 100, file);
-			weight = atof(buff);
-			if(weight != 0.0) {	
-				weights[i] = weight;	
-				if(i==9) {
-					i=0;
-				}
-				else {
-					i++;
-				}
+	char buff[100];
+	float weights[WEIGHTS];
+	float avgs[AVGS];
+	int cb_i = 0;
+	int avg_i = 0;
+	short stable = 1;
 
-				int j;
-				float avg = 0;
-				for(j=0; j < 10; j++) {
-					avg += weights[j];
-				}
-				printf("%f\n", avg/10);
+	while(fgets(buff, 100, file)) {
+		float weight;
+		fgets(buff, 100, file);
+		weight = atof(buff);
+		if(weight != 0.0) {	
+			weights[cb_i] = weight;	
+			if(cb_i==WEIGHTS-1) {
+				cb_i=0;
+			} else {
+				cb_i++;
 			}
 		}
+
+		int i;
+
+		avg=0;
+		for(i=0; i < WEIGHTS; i++) {
+			avg += weights[i];
+		}
+		avg = avg/WEIGHTS;
+
+		avgs[avg_i] = avg;	
+
+		if ( avg_i == 0 ) {
+			if ( fabs(avgs[9] - avgs[avg_i]) < DIFF) {
+				stable = 1;
+			} else{
+				stable = 0;
+			}
+		}
+		else {
+			if ( fabs(avgs[avg_i-1] - avgs[avg_i]) < DIFF) {
+				stable = 1;
+			} else{
+				stable = 0;
+			}
+		}
+
+		if ( !stable ) {
+			printf("unstable!\n");
+		}
+		else{
+			printf("\n");
+		}
+
+		if(avg_i==AVGS-1) {
+			avg_i=0;
+		} else {
+			avg_i++;
+		}
+
+
 	}
 
 	close(file);	
