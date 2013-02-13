b9a9662ef7febe0244cb1ce5887f152168c04781
root <root@localhost.localdomain>
2013-02-13 15:31:31 -0500

    Working circular buffer

--- src/read_serial.c
+++ src/read_serial.c
@@ -4,6 +4,7 @@
 
 int main(int argc, const char *argv[])
 {
+
 	FILE *file = fopen("/dev/ttyUSB0","r");
 	if(!file){
 		perror("Unable to open device");
@@ -15,20 +16,28 @@ int main(int argc, const char *argv[])
 	while(1){
 		char buff[100];
 		float weights[10];
-		float total = 0;
 		int i;
 		for(i=0; i<10; fgets(buff, 100, file)) {
 			float weight;
-
 			fgets(buff, 100, file);
 			weight = atof(buff);
 			if(weight != 0.0) {	
 				weights[i] = weight;	
-				total += weight;
-				i++;
+				if(i==9) {
+					i=0;
+				}
+				else {
+					i++;
+				}
+
+				int j;
+				float avg = 0;
+				for(j=0; j < 10; j++) {
+					avg += weights[j];
+				}
+				printf("%f\n", avg/10);
 			}
 		}
-		printf("%f\n", total/10);
 	}
 
 	close(file);	
