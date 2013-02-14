c246544bbb1129b8a94879a1acd3658c3b7f2643
root <root@localhost.localdomain>
2013-02-14 13:30:58 -0500

    Added calibration

--- src/read_serial.c
+++ src/read_serial.c
@@ -8,17 +8,24 @@
 #define DIFF  		0.01
 #define W_POWER 	0.27
 #define W_REMOTE 	0.40
-#define W_PAMPHLET 	0.23
+#define W_PAMPHLET 	0.22
 #define W_PAPER	 	0.06
-#define W_EMPTY		3.65
-#define ERROR		0.015
-#define STABLE_INTERVAL 30
+#define ERROR		0.01
+#define STABLE_INTERVAL 25
 
 float avg = 0;
 float W_MAX = 0;
+float W_EMPTY = 3.52;
 
 int main(int argc, const char *argv[])
 {
+	char buff[100];
+	float weights[WEIGHTS];
+	float avgs[AVGS];
+	int cb_i = 0;
+	int avg_i = 0;
+	short stable = 1;
+	float prev_weight = 0;
 
 	FILE *file = fopen("/dev/ttyUSB0","r");
 	if(!file){
@@ -28,13 +35,30 @@ int main(int argc, const char *argv[])
 
 	printf("Reading...\n");
 
-	char buff[100];
-	float weights[WEIGHTS];
-	float avgs[AVGS];
-	int cb_i = 0;
-	int avg_i = 0;
-	short stable = 1;
-	float prev_weight = 0;
+	char input[20];
+	printf("Calibrate? Y/N\n");
+	fgets(input,sizeof input, stdin);
+	if (input[0] == 'Y') {
+		printf("Empty the bin. Press enter to set weight\n");
+		fgets(input, sizeof input, stdin);
+		fgets(buff, 100, file);
+		float weight = atof(buff);
+		while (weight != 0) {
+			fgets(buff, 100, file);
+			weight = atof(buff);
+		}
+		W_EMPTY = weight;
+		printf("Add items to bin\n");
+		fgets(input, sizeof input, stdin);
+		fgets(buff, 100, file);
+		weight = atof(buff);
+		while (weight != 0) {
+			fgets(buff, 100, file);
+			weight = atof(buff);
+		}
+		W_MAX = weight;
+	}
+
 
 	while(fgets(buff, 100, file)) {
 		float weight;
@@ -47,89 +71,91 @@ int main(int argc, const char *argv[])
 			} else {
 				cb_i++;
 			}
-		}
+			int i;
 
-		int i;
-
-		avg=0;
-		for(i=0; i < WEIGHTS; i++) {
-			avg += weights[i];
-		}
-		avg = avg/WEIGHTS;
-		avgs[avg_i] = avg;	
-
-		for(i = 0; i < AVGS ; i++){
-
-			if ( i == 0 ) {
-				if ( fabs(avgs[AVGS-1] - avgs[i]) < DIFF) {
-					stable = 1;
-				} else{
-					stable = 0;
-					break;
-				}
+			avg=0;
+			for(i=0; i < WEIGHTS; i++) {
+				avg += weights[i];
 			}
-			else {
-				if ( fabs(avgs[i-1] - avgs[i]) < DIFF) {
-					stable = 1;
-				} else{
-					stable = 0;
-					break;
-				}
-			}
-		}
-		
-//		float stable_avg=0;
-//		for(i = 0; i < AVGS ; i++){
-//			stable_avg+=avgs[i];
-//		}
-//		stable_avg/= AVGS;
-//		if( fabs(stable_avg - avg) < DIFF) {
-//			stable = 1;
-//		} else {
-//			stable = 0;
-//		}
+			avg = avg/WEIGHTS;
+			avgs[avg_i] = avg;	
 
+			for(i = 0; i < AVGS ; i++){
 
-		int t;
-		if ( !stable ) {
-			t=0;
-		}
-		else{
-			if(t >= STABLE_INTERVAL) {
-				if (t == STABLE_INTERVAL) {
-					float percent_full = 0;
-					if ( prev_weight - avg  < -ERROR ) {
-						printf("\n\nAdded Weight! (%f lb)\n\n", avg-prev_weight);
-						W_MAX = avg;
-					} else if( prev_weight - avg < ERROR) {
-					} else if( prev_weight - avg < W_PAPER){
-						percent_full = (avg - W_EMPTY)*100 / (W_MAX-W_EMPTY);
-						printf("Paper picked (%d Percent Full)\n",(int)percent_full);
-					} else if( prev_weight - avg < W_PAMPHLET){
-						percent_full = (avg - W_EMPTY)*100/ (W_MAX-W_EMPTY);
-						printf("Pamphlet picked (%d Percent Full)\n",(int)percent_full);
-					} else if( prev_weight - avg < W_POWER){
-						percent_full = (avg - W_EMPTY)*100 / (W_MAX-W_EMPTY);
-						printf("Power cable picked (%d Percent Full)\n",(int)percent_full);
-					} else if( prev_weight - avg < W_REMOTE){
-						percent_full = (avg - W_EMPTY)*100 / (W_MAX-W_EMPTY);
-						printf("Remote picked (%d Percent Full)\n",(int)percent_full);
-					} else {
+				if ( i == 0 ) {
+					if ( fabs(avgs[AVGS-1] - avgs[i]) < DIFF) {
+						stable = 1;
+					} else{
+						stable = 0;
+						break;
 					}
 				}
-				prev_weight = avg;
-				t++;
+				else {
+					if ( fabs(avgs[i-1] - avgs[i]) < DIFF) {
+						stable = 1;
+					} else{
+						stable = 0;
+						break;
+					}
+				}
 			}
-			else {
-				t++;
-			}
-		}
+			
+	//		float stable_avg=0;
+	//		for(i = 0; i < AVGS ; i++){
+	//			stable_avg+=avgs[i];
+	//		}
+	//		stable_avg/= AVGS;
+	//		if( fabs(stable_avg - avg) < DIFF) {
+	//			stable = 1;
+	//		} else {
+	//			stable = 0;
+	//		}
 
 
-		if(avg_i==AVGS-1) {
-			avg_i=0;
-		} else {
-			avg_i++;
+			int t;
+			if ( !stable ) {
+				t=0;
+			}
+			else{
+				if(t >= STABLE_INTERVAL) {
+					if (t == STABLE_INTERVAL) {
+						float percent_full = 0;
+						if ( prev_weight - avg  < -ERROR ) {
+							printf("\n\nAdded Weight! (%f lb)\n\n", avg-prev_weight);
+							W_MAX = avg;
+							percent_full = (avg - W_EMPTY)*100 / (W_MAX-W_EMPTY);
+							printf("Paper picked (%d Percent Full)\n",(int)percent_full);
+						} else if( prev_weight - avg < ERROR) {
+						} else if( prev_weight - avg < W_PAPER){
+							percent_full = (avg - W_EMPTY)*100 / (W_MAX-W_EMPTY);
+							printf("Paper picked (%d Percent Full)\n",(int)percent_full);
+						} else if( prev_weight - avg < W_PAMPHLET){
+							percent_full = (avg - W_EMPTY)*100/ (W_MAX-W_EMPTY);
+							printf("Pamphlet picked (%d Percent Full)\n",(int)percent_full);
+						} else if( prev_weight - avg < W_POWER){
+							percent_full = (avg - W_EMPTY)*100 / (W_MAX-W_EMPTY);
+							printf("Power cable picked (%d Percent Full)\n",(int)percent_full);
+						} else if( prev_weight - avg < W_REMOTE){
+							percent_full = (avg - W_EMPTY)*100 / (W_MAX-W_EMPTY);
+							printf("Remote picked (%d Percent Full)\n",(int)percent_full);
+						} else {
+						}
+					}
+					prev_weight = avg;
+					t++;
+				}
+				else {
+					t++;
+				}
+			}
+
+
+			if(avg_i==AVGS-1) {
+				avg_i=0;
+			} else {
+				avg_i++;
+			}
+
 		}
 
 
