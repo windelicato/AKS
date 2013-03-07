cea8c615a039e0395fb6c61e14231c015fff7497
wei001 <wei001@bucknell.edu>
2013-03-07 17:39:02 +0000

    Added packet tokenizer

--- tests/olp_recv_test.c
+++ tests/olp_recv_test.c
@@ -16,6 +16,25 @@ int main (int argc, const char *argv[])
 		memset(message_recv, '\0', 9*MAXBUFLEN);
 
 		get_msg(argv[1], message_recv);
+
+		char tokens[MAXBUFLEN][12];
+		char *next_str;
+		strcpy(tokens[0],strtok(message_recv, " "));
+
+		int i;
+		for(i=1; i<MAXBUFLEN; i++) {
+			if ((next_str = strtok(NULL," ")) != NULL){
+				strcpy(tokens[i], next_str);
+			}
+			else
+				strcpy(tokens[i], "\0");
+		}
+
+		for (i=0; i<MAXBUFLEN; i++) {
+			printf("TOKEN %d: %s\n", i, tokens[i]);
+		}
+
+		message_send = message_recv;
 		send_msg(argv[1], message_send);
 	}
 	return 0;
