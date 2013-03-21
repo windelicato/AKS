5584f975d90f57a4724afa12ca077aab328323a6
wei001 <wei001@bucknell.edu>
2013-03-21 15:58:44 +0000

    Updated networking

--- include/comm.h
+++ include/comm.h
@@ -1,6 +1,21 @@
 #ifndef _COMM_H_
 #define _COMM_H_
 #include<sys/socket.h>
+#include<semaphore.h>
+
+struct network_data{
+	char msg_recv[1000];
+	char msg_send[1000];
+	sem_t lock_send;
+	sem_t lock_recv;
+};
+
+void set_mesg_send(struct network_data *data, char* buff);
+void get_mesg_send(struct network_data *data, char* buff);
+int network_init();
+void *server_daemon(void *arg);
+
+
 
 void *get_in_addr(struct sockaddr *sa);
 
